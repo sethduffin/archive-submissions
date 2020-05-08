05a8b8ab7a63e6e9041b70c1cc2f7696e0009780
Seth Duffin <seth.duffin@atomicjolt.com>
2020-05-08 00:01:02 -0600

    Fixed username and password storage

--- code/local.py
+++
@@ -1,5 +0,0 @@
-username = ""			# Canvas username that has access to all assginment links
-password = ""			# Canvas password to username
-
-output_folder = "submissions"	# Folder name to save archived submissons to
-clear_output_folder = True 		# Removes all files in output folder when script is executed 
